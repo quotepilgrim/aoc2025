15f437cfbc89fb92499e1d8e1bfa17e73fd59a86
Quote Pilgrim <caio.psv@gmail.com>
2025-12-03 09:26:44 -0300

    d3 done, can already tell p2 is gonna kick my butt

---
+++ d3.lua
@@ -0,0 +1,60 @@
+local t = {}
+local result
+local data = {}
+
+local function p1()
+	result = 0
+	for _, s in ipairs(data) do
+		local max = 0
+		local max_pos = 1
+		local jolts = ""
+
+		for i = 1, #s - 1 do
+			local digit = assert(tonumber(string.sub(s, i, i)))
+			if digit > max then
+				max = digit
+				max_pos = i
+			end
+		end
+
+		jolts = jolts .. tostring(max)
+		max = 0
+
+		for i = max_pos + 1, #s do
+			local digit = assert(tonumber(string.sub(s, i, i)))
+			if digit > max then
+				max = digit
+				max_pos = i
+			end
+		end
+
+		jolts = jolts .. tostring(max)
+		result = result + tonumber(jolts)
+	end
+end
+
+local function p2()
+	--
+end
+
+function t.load(part, filename)
+	local file = assert(io.open(filename))
+
+	for line in file:lines() do
+		table.insert(data, line)
+	end
+
+	if part == 1 then
+		p1()
+	elseif part == 2 then
+		p2()
+	end
+
+	return result
+end
+
+function t.draw()
+	love.graphics.print(result or "")
+end
+
+return t
