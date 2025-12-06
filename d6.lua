d215d32f29a0d045dd0e120385495a5106fcffd7
Quote Pilgrim <caio.psv@gmail.com>
2025-12-06 14:11:32 -0300

    d6p2 donzo!!
    I might have been able to do this without having to create a new table
    for the transformed data but it seemed simpler to do it this way.

--- d6.lua
+++ d6.lua
@@ -25,6 +25,50 @@ end
 
 local function p2(data)
 	local result = 0
+	local transform = {}
+
+	local function add(tbl)
+		local sum = 0
+		for _, v in ipairs(tbl) do
+			sum = sum + v
+		end
+		return sum
+	end
+
+	local function multiply(tbl)
+		local prod = 1
+		for _, v in ipairs(tbl) do
+			prod = prod * v
+		end
+		return prod
+	end
+
+	for i = 1, #data[1] do
+		local col = ""
+		for j = 1, #data do
+			local v, _ = data[j]:sub(i, i):gsub(" ", "")
+			col = col .. v
+		end
+		if col ~= "" then
+			table.insert(transform, col)
+		end
+	end
+
+	local problem = {}
+	for i = #transform, 1, -1 do
+		local s = transform[i]
+		if s:sub(#s, #s) == "*" then
+			table.insert(problem, tonumber(s:sub(1, #s - 1)))
+			result = result + multiply(problem)
+			problem = {}
+		elseif s:sub(#s, #s) == "+" then
+			table.insert(problem, tonumber(s:sub(1, #s - 1)))
+			result = result + add(problem)
+			problem = {}
+		else
+			table.insert(problem, tonumber(s))
+		end
+	end
 	return result
 end
 
@@ -32,18 +76,20 @@ function t.load(part, filename)
 	local file = assert(io.open(filename))
 	local data = {}
 
-	for line in file:lines() do
-		local row = {}
-		local matches = line:gmatch("[^%s]+")
-		for match in matches do
-			table.insert(row, match)
-		end
-		table.insert(data, row)
-	end
-
 	if part == 1 then
+		for line in file:lines() do
+			local row = {}
+			local matches = line:gmatch("[^%s]+")
+			for match in matches do
+				table.insert(row, match)
+			end
+			table.insert(data, row)
+		end
 		result = p1(data)
 	elseif part == 2 then
+		for line in file:lines() do
+			table.insert(data, line)
+		end
 		result = p2(data)
 	end
 
